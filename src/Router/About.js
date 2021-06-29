95308daa4648cf1799cad1fe7a668f2c783b1b91
bhattamad <bhattamad@FINLHYDAG884821>
2021-06-29 15:32:55 +0530

    changes

---
+++ src/Router/About.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            
+            <h1>This is cloned by Madhu</h1>
+         );
+    }
+}
+ 
+export default About;
